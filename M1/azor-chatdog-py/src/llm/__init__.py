643b666d8edcbf1ddc35ee2df2315c272cee4afa
AlanLes <a.leszczuk94@gmail.com>
2025-11-17 05:32:16 +0100

    zadanie 12

--- M1/azor-chatdog-py/src/llm/__init__.py
+++ M1/azor-chatdog-py/src/llm/__init__.py
@@ -5,6 +5,6 @@ Provides LLM client classes for dynamic initialization.
 
 from .gemini_client import GeminiLLMClient
 from .llama_client import LlamaClient
-
+from .anthropic_client import AnthropicLLMClient
 # Export client classes for dynamic initialization
-__all__ = ['GeminiLLMClient', 'LlamaClient']
+__all__ = ['GeminiLLMClient', 'LlamaClient', 'AnthropicLLMClient']
