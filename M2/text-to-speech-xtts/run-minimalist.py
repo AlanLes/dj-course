8a7e67d80f68f5f805490bed9b3655a962538ccc
AlanLes <a.leszczuk94@gmail.com>
2025-11-27 06:15:16 +0100

    zadanie M2 2.1 dodatek dragonikowy

--- M2/text-to-speech-xtts/run-minimalist.py
+++ M2/text-to-speech-xtts/run-minimalist.py
@@ -1,15 +1,25 @@
 from TTS.api import TTS
+import torch
+# Monkeypatch torch.load to allow loading custom classes (required for XTTS)
+original_load = torch.load
+def safe_load(*args, **kwargs):
+    kwargs['weights_only'] = False
+    return original_load(*args, **kwargs)
+torch.load = safe_load
+
 
 tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2",progress_bar=True).to("cpu")
-FILE_PATH = "sample-agent.wav"
-OUTPUT_WAV_PATH = "output.wav"
+FILE_PATH = "smpl-dragon.wav"
+OUTPUT_WAV_PATH = "output-minimalist.wav"
 
-text = "Lubię ser i bułki - i strzelam z dwururki!"
-# text = "Lubię ser i bułki - i strzelam z dwururki! Lubię ser i bułki - i strzelam z dwururki! Lubię ser i bułki - i strzelam z dwururki! Lubię ser i bułki - i strzelam z dwururki! Lubię ser i bułki - i strzelam z dwururki! Lubię ser i bułki - i strzelam z dwururki! Lubię ser i bułki - i strzelam z dwururki! Lubię ser i bułki - i strzelam z dwururki! Lubię ser i bułki - i strzelam z dwururki! Lubię ser i bułki - i strzelam z dwururki! Lubię ser i bułki - i strzelam z dwururki! Lubię ser i bułki - i strzelam z dwururki! Lubię ser i bułki - i strzelam z dwururki!"
+text = "Hello darkness, my old friend,\nI've come to talk with you again,\nBecause a vision softly creeping,\nLeft its seeds while I was sleeping,\nAnd the vision that was planted in my brain\nStill remains"
+# text = "Merhaba karanlık, eski dostum, Seninle tekrar konuşmaya geldim, Çünkü yavaşça sürünen bir görüntü, Uyurken tohumlarını bıraktı, Ve beynime ekilen görüntü Hâlâ duruyor"
+
 tts.tts_to_file(
     text=text,
     file_path=OUTPUT_WAV_PATH,
     speaker_wav=FILE_PATH,
-    language="pl"
+    language="en",
+    speed=2.0
 )
 
